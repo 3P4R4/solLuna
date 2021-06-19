9d09983fe094c9867efd8995f7f8e234b96b64ca
lenin jose mendoza espina <mendoza124302@gmail.com>
2021-06-18 22:10:33 -0500

    commit

---
+++ jScript.js
@@ -0,0 +1,6 @@
+document.querySelector(".container").addEventListener("click", () => {
+    document.querySelector(".sol-logo").classList.toggle("animar-sol");
+    document.querySelector(".luna-logo").classList.toggle("animar-luna");
+    document.querySelector("body").classList.toggle("oscuro");
+
+})
